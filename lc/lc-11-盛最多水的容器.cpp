186ba3faa77d7441d73b50f7190d916889168e1a
233 <57021809+qwq2519@users.noreply.github.com>
2026-02-15 00:01:12 +0800

    add lc-11-盛最多水的容器

---
+++ lc/lc-11-盛最多水的容器.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+  int maxArea(vector<int> &height) {
+    int l = 0, r = height.size() - 1;
+    int ans = (r - l) * min(height[l], height[r]);
+    while (l < r) {
+      if (height[l] < height[r]) {
+        ++l;
+      } else {
+        --r;
+      }
+      ans = max(ans, (r - l) * min(height[l], height[r]));
+    }
+    return ans;
+  }
+};
