5b432e64703f3fad5d5468e7369b934af27cc9ca
233 <57021809+qwq2519@users.noreply.github.com>
2026-02-15 12:43:16 +0800

    1

--- lc/3-无重复字符最长子串.cpp
+++ lc/3-无重复字符最长子串.cpp
@@ -1,4 +1,24 @@
 class Solution {
 public:
-  int lengthOfLongestSubstring(string s) {}
+  int lengthOfLongestSubstring(string s) {
+    int n = s.size();
+    if (s.empty()) {
+      return 0;
+    }
+    int ans = 1;
+
+    array<int, 256> last;
+    last.fill(-1);
+
+    int l = 0;
+    for (int r = 0; r < n; ++r) {
+      unsigned char c = s[r];
+      if (last[c] >= l)
+        l = last[c] + 1;
+      last[c] = r;
+      ans = max(ans, r - l + 1);
+    }
+
+    return ans;
+  }
 };
