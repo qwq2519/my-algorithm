cbb18661cb30b0d6c8fcb59260af3974c9dd9d3d
233 <57021809+qwq2519@users.noreply.github.com>
2026-02-16 13:01:24 +0800

    add lc-142-环形链表2

---
+++ lc/142-环形链表2.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  ListNode *detectCycle(ListNode *head) {
+    if (!head) {
+      return nullptr;
+    }
+    auto fast = head;
+    auto slow = head;
+    while (fast && fast->next) {
+      fast = fast->next->next;
+      slow = slow->next;
+      if (slow == fast) {
+        auto p = head;
+        while (slow != p) {
+          p = p->next;
+          slow = slow->next;
+        }
+        return p;
+      }
+    }
+
+    return nullptr;
+  }
+};
