588b16f5035d1f70e647f3bc1a7cf6dfd7f50c3e
233 <57021809+qwq2519@users.noreply.github.com>
2026-02-24 11:07:14 +0800

    update lc-912-排序数组 add mergeSort

--- lc/912-排序数组.cpp
+++ lc/912-排序数组.cpp
@@ -1,8 +1,11 @@
+#include <algorithm>
 class Solution {
 public:
   vector<int> sortArray(vector<int> &nums) {
     srand(time(0));
-    Quicksort(nums, 0, nums.size() - 1);
+    // Quicksort(nums, 0, nums.size() - 1);
+    vector<int> tmp(nums.size(), 0);
+    mergeSort(nums, tmp, 0, nums.size() - 1);
     return nums;
   }
 
@@ -33,5 +36,23 @@ public:
     }
     Quicksort(a, l, i - 1);
     Quicksort(a, j + 1, r);
+  }
+
+  void mergeSort(vector<int> &a, vector<int> &tmp, int l, int r) {
+    if (l >= r)
+      return;
+    int m = (l + r) / 2;
+    mergeSort(a, tmp, l, m);
+    mergeSort(a, tmp, m + 1, r);
+    int i = l, j = m + 1;
+    int k = l;
+    while (i <= m && j <= r)
+      tmp[k++] = (a[i] < a[j] ? a[i++] : a[j++]);
+    while (i <= m)
+      tmp[k++] = a[i++];
+    while (j <= r)
+      tmp[k++] = a[j++];
+    for (int t = l; t <= r; t++)
+      a[t] = tmp[t];
   }
 };
