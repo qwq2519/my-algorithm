572729e706000c1087b52d1d24f0ec7c6f9097a8
233 <57021809+qwq2519@users.noreply.github.com>
2026-02-25 21:44:20 +0800

    add lc-118-杨辉三角

---
+++ lc-hot100/118-杨辉三角.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+  vector<vector<int>> generate(int numRows) {
+    vector<vector<int>> v;
+    v.reserve(numRows);
+
+    for (int i = 0; i < numRows; i++) {
+      vector<int> row;
+      for (int j = 0; j <= i; j++) {
+        if (j == 0 || j == i)
+          row.push_back(1);
+        else
+          row.push_back(v[i - 1][j - 1] + v[i - 1][j]);
+      }
+      v.push_back(row);
+    }
+    return v;
+  }
+};
