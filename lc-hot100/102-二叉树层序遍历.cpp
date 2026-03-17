797f62c6ef8652aafd620e649129c7054e09da5c
233 <57021809+qwq2519@users.noreply.github.com>
2026-03-17 13:39:11 +0800

    lc-102 二叉树层序遍历 dfs && bfs

--- lc-hot100/102-二叉树层序遍历.cpp
+++ lc-hot100/102-二叉树层序遍历.cpp
@@ -11,31 +11,55 @@
  * };
  */
 class Solution {
-public:
-  vector<vector<int>> levelOrder(TreeNode *root) {
-    queue<pair<TreeNode *, int>> q;
-    vector<vector<int>> ans;
-    if (!root) {
-      return ans;
+
+    vector<vector<int>> bfs(TreeNode *root) {
+        queue<pair<TreeNode *, int>> q;
+        vector<vector<int>> ans;
+        if (!root) {
+            return ans;
+        }
+
+        q.push({root, 1});
+        while (!q.empty()) {
+            auto [node, dep] = q.front();
+            q.pop();
+
+            if (ans.size() < dep) {
+                ans.push_back({});
+            }
+            ans[dep - 1].push_back(node->val);
+
+            if (node->left) {
+                q.push({node->left, dep + 1});
+            }
+            if (node->right) {
+                q.push({node->right, dep + 1});
+            }
+        }
+        return ans;
     }
 
-    q.push({root, 1});
-    while (!q.empty()) {
-      auto [node, dep] = q.front();
-      q.pop();
-
-      if (ans.size() < dep) {
-        ans.push_back({});
-      }
-      ans[dep - 1].push_back(node->val);
-
-      if (node->left) {
-        q.push({node->left, dep + 1});
-      }
-      if (node->right) {
-        q.push({node->right, dep + 1});
-      }
+    void dfs(TreeNode *p, int dep, vector<vector<int>> &ans) {
+        if (!p) {
+            return;
+        }
+        if (ans.size() == dep) {
+            ans.push_back({});
+        }
+        ans[dep].push_back(p->val);
+        dfs(p->left, dep + 1, ans);
+        dfs(p->right, dep + 1, ans);
     }
-    return ans;
-  }
+
+    vector<vector<int>> dfs(TreeNode *p) {
+        vector<vector<int>> ans;
+        dfs(p, 0, ans);
+        return ans;
+    }
+
+  public:
+    vector<vector<int>> levelOrder(TreeNode *root) {
+        // return bfs(root);
+        return dfs(root);
+    }
 };
