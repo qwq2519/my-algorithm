72e073706caf5617fdec498db7f89679b2ace238
233 <57021809+qwq2519@users.noreply.github.com>
2026-03-19 20:20:35 +0800

    add lc-209-长度最小子数组

--- lc/lc-150/209-长度最小的子数组.cpp
+++ lc/lc-150/209-长度最小的子数组.cpp
@@ -1,6 +1,6 @@
 class Solution {
-  public:
-    int minSubArrayLen(int target, vector<int> &nums) {
+
+    int twoPoiner(int target, vector<int> &nums) {
         int ans = 1e9;
         int n = nums.size();
         int l = 0;
@@ -14,5 +14,46 @@ class Solution {
             }
         }
         return ans == 1e9 ? 0 : ans;
+    }
+
+    int binarySearch(int target, vector<int> &nums) {
+        // 题目还要求一种n log
+        // n的，并且元素都是正数，可以考虑前缀和二分。如果不能确保是正数，就是lc862
+        // 单调队列
+        int ans = 1e9;
+        int n = nums.size();
+        vector<int> pre(n + 1, 0);
+        for (int i = 0; i < n; i++)
+            pre[i + 1] = pre[i] + nums[i];
+
+        // 假设子数组以i结尾，向前找个j，使得pre[i]-pre[j]>=target,
+        // pre[i]-target >= pre[j]  ,让j尽量大即可
+        for (int i = 1; i <= n; i++) {
+            int need = pre[i] - target;
+            int l = -1, r = i;
+            while (l + 1 < r) {
+                int mid((l + r) >> 1);
+                if (pre[mid] <= need)
+                    l = mid;
+                else
+                    r = mid;
+            }
+            if (l != -1) {
+                ans = min(ans, i - l);
+            }
+        }
+        //  for (int i = 0; i < n; i++) {
+        //     int need = pre[i] + target;
+        //     int j = lower_bound(pre.begin() + i + 1, pre.end(), need) -
+        //     pre.begin(); if (j <= n) {
+        //         ans = min(ans, j - i);
+        //     }
+        // }
+        // return ans == 1e9 ? 0 : ans;
+    }
+
+  public:
+    int minSubArrayLen(int target, vector<int> &nums) {
+        return binarySearch(target, nums);
     }
 };
