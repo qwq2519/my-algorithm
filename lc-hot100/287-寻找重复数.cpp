d24dee3d3a3758b2be65deebf05639d1147894f5
233 <57021809+qwq2519@users.noreply.github.com>
2026-04-16 10:51:21 +0800

    add lc-287-寻找重复数 原地哈希

--- lc-hot100/287-寻找重复数.cpp
+++ lc-hot100/287-寻找重复数.cpp
@@ -1,20 +1,34 @@
+// class Solution {
+//   public:
+//     int findDuplicate(vector<int> &nums) {
+//         // n+1个数，值为[1,n],  可以构成环
+//         int slow = 0, fast = 0;
+//         while (1) {
+//             slow = nums[slow];
+//             fast = nums[nums[fast]];
+//             if (slow == fast) {
+//                 int head = 0;
+//                 while (head != slow) {
+//                     head = nums[head];
+//                     slow = nums[slow];
+//                 }
+//                 return head;
+//             }
+//         }
+//         return slow;
+//     }
+// };
 class Solution {
   public:
     int findDuplicate(vector<int> &nums) {
-        // n+1个数，值为[1,n],  可以构成环
-        int slow = 0, fast = 0;
-        while (1) {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-            if (slow == fast) {
-                int head = 0;
-                while (head != slow) {
-                    head = nums[head];
-                    slow = nums[slow];
-                }
-                return head;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            int idx = abs(nums[i]) - 1;
+            if (nums[idx] < 0) {
+                return idx + 1;
             }
+            nums[idx] = -nums[idx];
         }
-        return slow;
+        return 0;
     }
 };
